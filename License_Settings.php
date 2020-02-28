6a5f9b0fd10dad98ef4f278a9e60439495779e9d
Hung Vu <vunamhung@gmail.com>
2020-02-28 20:45:22 +0700

    :recycle: Refactoring code.

--- License_Settings.php
+++ License_Settings.php
@@ -4,11 +4,26 @@ namespace vnh;
 
 class License_Settings extends Register_Settings {
 	private $license;
+	public $license_status;
+	public $license_status_option_name;
+	public $license_status_message;
 
 	public function __construct(License_Management $license) {
 		$this->license = $license;
 		$this->prefix = $license->args['slug'];
 		$this->option_name = 'license';
+
+		$this->license_status_option_name = $this->get_prefix() . '_license_status';
+		$this->license_status = get_option($this->license_status_option_name);
+		$this->license_status_message = [
+			'empty_key' => esc_html__('The entered license key is not valid.', 'vnh_textdomain'),
+			'invalid' => esc_html__('The entered license key is not valid.', 'vnh_textdomain'),
+			'expired' => esc_html__('Your key has expired and needs to be renewed.', 'vnh_textdomain'),
+			'inactive' => esc_html__('Your license key is valid, but is not active.', 'vnh_textdomain'),
+			'disabled' => esc_html__('Your license key is currently disabled. Please contact support.', 'vnh_textdomain'),
+			'site_inactive' => esc_html__('Your license key is valid, but not active for this site.', 'vnh_textdomain'),
+			'valid' => esc_html__('Your license key is valid and active for this site.', 'vnh_textdomain'),
+		];
 	}
 
 	public function register_setting_fields() {
@@ -34,7 +49,7 @@ class License_Settings extends Register_Settings {
 	}
 
 	public function render() {
-		$this->license->check_license();
+		$this->check_license();
 		return parent::render();
 	}
 
@@ -55,28 +70,53 @@ class License_Settings extends Register_Settings {
 		request($this->license->args['remote_api_url'], [
 			'body' => [
 				'edd_action' => $action,
-				'license' => $this->license->args['license_key'],
+				'license' => $this->license->license_key,
 				'item_id' => $this->license->args['item_id'],
 				'url' => home_url('/'),
 			],
 		]);
 	}
 
+	public function check_license() {
+		update_option($this->license_status_option_name, $this->get_license_status());
+	}
+
+	private function get_license_status() {
+		if (empty($this->license->license_key)) {
+			return 'empty_key';
+		}
+
+		$license_data = request($this->license->args['remote_api_url'], [
+			'body' => [
+				'edd_action' => 'check_license',
+				'license' => $this->license->license_key,
+				'item_id' => $this->license->args['item_id'],
+				'url' => home_url('/'),
+			],
+		]);
+
+		if (is_wp_error($license_data)) {
+			return false;
+		}
+
+		return $license_data['license'];
+	}
+
 	public function display_field_license_action($field, $option) {
-		$status = get_option($this->license->license_status_option_name);
+		$status = get_option($this->license_status_option_name);
 
 		if ($status === 'inactive' || $status === 'site_inactive') {
 			$output = sprintf(
 				'<input type="submit" name="activate_key" class="button-secondary" value="Activate Site" /><p style="color:#ffb900;">%s</p>',
-				$this->license->license_status_message[$status]
+				$this->license_status_message[$status]
 			);
 		} elseif ($status === 'valid') {
 			$output = sprintf(
 				'<input type="submit" name="deactivate_key" class="button-secondary" value="Deactivate Site" /><p style="color:green;">%s</p>',
-				$this->license->license_status_message[$status]
+				$this->license_status_message[$status]
 			);
 		} else {
-			$output = sprintf('<p style="color:red;">%s</p>', $this->license->license_status_message[$status]);
+			$output = sprintf('<p style="color:red;">%s</p>', $this->license_status_message[$status]);
 		}
 
 		echo $output;
