50258c5a579221b7b715e627f35088edf85f30e3
Hung Vu <vunamhung@gmail.com>
2020-02-28 20:45:22 +0700

    :recycle: Refactoring code.

--- Plugin_Updater.php
+++ Plugin_Updater.php
@@ -6,22 +6,14 @@ use stdClass;
 use vnh\contracts\Bootable;
 
 class Plugin_Updater implements Bootable {
-	private $license_key;
-	private $plugin_slug;
+	private $license;
 	private $response_key;
 	private $plugin_base;
-	public $remote_api_url;
-	public $item_id;
-	public $version;
 
-	public function __construct($args, License_Settings $settings) {
-		$this->license_key = trim($settings->get_option('key'));
-		$this->plugin_slug = basename($args['plugin_file'], '.php');
-		$this->response_key = $this->plugin_slug . '-update-response';
-		$this->plugin_base = plugin_basename($args['plugin_file']);
-		$this->remote_api_url = $args['remote_api_url'];
-		$this->item_id = $args['item_id'];
-		$this->version = $args['version'];
+	public function __construct(License_Management $license) {
+		$this->license = $license;
+		$this->response_key = $license->args['slug'] . '-update-response';
+		$this->plugin_base = plugin_basename($license->args['plugin_file']);
 	}
 
 	public function boot() {
@@ -69,21 +61,21 @@ class Plugin_Updater implements Bootable {
 		$update_data = get_transient($this->response_key);
 
 		if ($update_data === false) {
-			if (empty($this->license_key)) {
+			if (empty($this->license->license_key)) {
 				return false;
 			}
 
-			$update_data = request($this->remote_api_url, [
+			$update_data = request($this->license->args['remote_api_url'], [
 				'body' => [
 					'edd_action' => 'get_version',
-					'license' => $this->license_key,
-					'item_id' => $this->item_id,
+					'license' => $this->license->license_key,
+					'item_id' => $this->license->args['item_id'],
 				],
 			]);
 
 			// If the response failed, try again in 30 minutes
 			if (is_wp_error($update_data)) {
-				$data['new_version'] = $this->version;
+				$data['new_version'] = $this->license->args['version'];
 				set_transient($this->response_key, $data, MINUTE_IN_SECONDS * 30);
 
 				return false;
@@ -95,7 +87,7 @@ class Plugin_Updater implements Bootable {
 			set_transient($this->response_key, $update_data, HOUR_IN_SECONDS * 12);
 		}
 
-		if (version_compare($this->version, $update_data['new_version'], '>=')) {
+		if (version_compare($this->license->args['version'], $update_data['new_version'], '>=')) {
 			return false;
 		}
 
